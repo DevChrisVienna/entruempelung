5925f9684911db2b37338f0a170d021e0d3bf0e3
Rocal007 <r.sauer007@gmail.com>
2021-12-28 21:28:30 +0100

    rethink of cat and posts

--- app/public/wp-content/themes/local_seo/includes/custom-functions.php
+++ app/public/wp-content/themes/local_seo/includes/custom-functions.php
@@ -12,7 +12,7 @@
 
 
 /**
- * caterogies for pages
+ * taxoniomies for pages
  */
 function add_taxonomies_to_pages() {
     register_taxonomy_for_object_type( 'post_tag', 'page' );
@@ -47,6 +47,9 @@ function create_page($title_of_the_page,$content,$parent_id = NULL )
         return $objPage->ID;
     }
     
+    $cat=get_cat_ID( 'test' );
+    echo $cat;
+
     $page_id = wp_insert_post(
             array(
             'comment_status' => 'close',
@@ -57,17 +60,48 @@ function create_page($title_of_the_page,$content,$parent_id = NULL )
             'post_status'    => 'publish',
             'post_content'   => $content,
             'post_type'      => 'page',
-            'post_parent'    =>  $parent_id //'id_of_the_parent_page_if_it_available'
+            'post_parent'    =>  $parent_id, //'id_of_the_parent_page_if_it_available'
             )
         );
     echo "Created page_id=". $page_id." for page '".$title_of_the_page. "'<br/>";
     return $page_id;
 }
 
-create_page( 'How it works', 'This is how it works');
-create_page( 'Contact Us', 'The contact us page');
-create_page( 'About Us', 'The about us page');
-create_page( 'Team', 'The team page');
-$pid = create_page( 'Sample Page', 'This is sample page');
-create_page( 'Sample SubPage 1', 'This is sample SubPage 1',$pid);
-create_page( 'Sample SubPage 2', 'This is sample SubPage 2',$pid);
+
+
+create_page( 'Über uns', '');
+create_page( 'Impressum', '');
+create_page( 'AGB', '');
+
+
+
+function create_post($title_of_the_page,$content,$cat_id = NULL ) 
+{
+    $objPage = get_page_by_title($title_of_the_page, 'OBJECT', 'post');
+    if( ! empty( $objPage ) )
+    {
+        //echo "Page already exists:" . $title_of_the_page . "<br/>";
+        return $objPage->ID;
+    }
+    
+    $page_id = wp_insert_post(
+            array(
+            'comment_status' => 'close',
+            'ping_status'    => 'close',
+            'post_author'    => 1,
+            'post_title'     => ucwords($title_of_the_page),
+            'post_name'      => strtolower(str_replace(' ', '-', trim($title_of_the_page))),
+            'post_status'    => 'publish',
+            'post_content'   => $content,
+            'post_type'      => 'post',
+            'post_category' => array($cat_id),
+            )
+        );
+    echo "Created page_id=". $page_id." for page '".$title_of_the_page. "'<br/>";
+    return $page_id;
+}
+
+$business_field = 'Entrümpelungen';
+$location = '1010 Wien'; 
+
+create_post( $business_field . ' ' . $location , '', '2');
